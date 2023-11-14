b256017bb0451b181a558680fc8ecc2d849298e0
zecamilplano <ezequiasgama2@gmail.com>
2023-11-14 16:04:25 -0300

    Entrada e saida de dados

---
+++ 3-Entrada-de-Dados/index-c.cpp
@@ -0,0 +1,12 @@
+#include <cmath>
+#include <cstdlib>
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+  int a, b;
+  scanf("%d %d", &a, &b);
+  double media;
+  printf("A media de %d e %d é %f\n", a, b, media);
+  return EXIT_SUCCESS;
+}
---
+++ 3-Entrada-de-Dados/index-cpp.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+  int valor;
+
+  cout << "Qual valor? ";
+  cin >> valor;
+  cout << "O valor digitado foi " << valor << endl;
+
+  return 0;
+}
