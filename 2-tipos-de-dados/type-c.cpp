081e03fd2531ecefd0c92c4869c9de032a6ca8d9
zecamilplano <ezequiasgama2@gmail.com>
2023-11-14 16:00:41 -0300

    Aprendendo tipos de Saida

---
+++ 2-tipos-de-dados/type-c.cpp
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(void) {
+  char nome[30] = "Ezequiel";
+
+  printf("O valor do nome é %s\n", nome);
+
+  return 0;
+}
---
+++ 2-tipos-de-dados/type-cpp.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+
+  int numero = 10;
+  cout << "O numero é " << numero << endl;
+
+  float pi = 3.1415;
+  cout << "O valor de pí é " << pi << endl;
+
+  double numero_maior = 99999999.3333333333;
+  cout << "O numero gigante e" << numero_maior << endl;
+
+  char letra = 'A';
+  cout << "A letra é " << letra << endl;
+
+  string nome = "Ezequiel";
+  cout << "O nome é " << nome << endl;
+
+  return 0;
+}
