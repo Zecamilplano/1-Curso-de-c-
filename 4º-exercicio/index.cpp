0bf2f9eabae69f68adbd99f2c7946cc3f52da832
zecamilplano <ezequiasgama2@gmail.com>
2023-11-14 17:02:34 -0300

    Esse foi meu jeito de rosolver

---
+++ 4º-exercicio/index.cpp
@@ -0,0 +1,20 @@
+// Faça um programa que: vai perguntar 2 varíaveis a e b depois imprima os
+// valores invertido
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  int a;
+  int b;
+
+  cout << "Valor de A ";
+  cin >> a;
+  cout << "Valor de B ";
+  cin >> b;
+
+  cout << "O valor de A é " << b << endl;
+  cout << "E o valor de B é " << a << endl;
+  return 0;
+}
