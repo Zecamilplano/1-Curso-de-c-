9ccdd196e710e94598867414a1fbbbf543a10c8b
zecamilplano <ezequiasgama2@gmail.com>
2023-11-14 15:59:07 -0300

    Aprendendo dar hello-world em c++

---
+++ 1-hello-world/olamundo.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  cout << "Olá Mundo" << endl;
+  return 0;
+}
+
+// Em c
+/*#include "stdio.h"
+
+int main(void) {
+  puts("Olá Mundo");
+  return 0;
+} */
+
+// Em c++
